1cdfd3336c4cc849507053750866a43a4942aa26
Melfu <degardenzi.clement@gmail.com>
2024-01-04 13:34:41 +0100

    init router

--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -4,8 +4,7 @@ import Home from '../src/pages/home/home.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Home />
+    </React.StrictMode>,
 );
-
